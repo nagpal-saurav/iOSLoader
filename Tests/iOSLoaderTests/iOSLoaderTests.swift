07936308c8c858a290929e6121aecaa25f092cf8
Saurav Nagpal <nagpalsaurav@gmail.com>
2023-03-01 15:02:47 +0530

    first commit
    First commit.

---
+++ Tests/iOSLoaderTests/iOSLoaderTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import iOSLoader
+
+final class iOSLoaderTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(iOSLoader().text, "Hello, World!")
+    }
+}
